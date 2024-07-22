02027414668b14110080557692da3c3d6e23042b
fajrCode <fajri42vandi@gmail.com>
2024-07-22 19:57:54 +0700

    setup breeze

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,12 +24,19 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
+            'username' => fake()->unique()->userName,
             'password' => static::$password ??= Hash::make('password'),
+            'role' => 'warga',
+            'status' => true,
             'remember_token' => Str::random(10),
         ];
+    }
+
+    public function admin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => 'admin',
+        ]);
     }
 
     /**
