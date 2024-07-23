66eb673b45e92f9f3ba8511484357c0a1317e15b
MRoyhanF <froyhan0@gmail.com>
2024-07-23 23:34:53 +0700

    setup desain admin page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -151,11 +151,12 @@
     </main>
 
     <!-- ======= Footer ======= -->
-    <footer id="footer" class="footer">
+    {{-- <footer id="footer" class="footer">
         <div class="copyright">
         &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
         </div>
-    </footer><!-- End Footer -->
+    </footer> --}}
+    <!-- End Footer -->
 
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
