c33845143edb0932afc14389c1e4ca2456e0ba11
fajrCode <fajri42vandi@gmail.com>
2024-08-07 17:03:20 +0700

    feat: detail aset admin

--- app/Http/Controllers/Admin/AsetController.php
+++ app/Http/Controllers/Admin/AsetController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Models\Pbb;
 use App\Models\Aset;
 use App\Models\Warga;
+use App\Models\Sporadik;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Vinkla\Hashids\Facades\Hashids;
@@ -12,59 +14,78 @@ class AsetController extends Controller
 {
     public function index()
     {
-        $asets = Aset::all();
-        return view('pages.admin.aset.list', compact('asets'));
+        try {
+            $asets = Aset::all();
+            return view('pages.admin.aset.list', compact('asets'));
+        } catch (\Throwable $th) {
+            return redirect()->route('admin.dashboard')->with('error', 'Server Error');
+        }
     }
 
     public function detail($id)
     {
-        $aset = Aset::all();
-        return view('pages.admin.aset.detail', compact('aset', 'id'));
+        $unhashed = Hashids::decode($id)[0];
+        $aset = Aset::with('warga')->find($unhashed);
+        $pbb = Pbb::with('aset')->where('aset_id', $unhashed)->get();
+        $sporadik = Sporadik::with('aset')->where('aset_id', $unhashed)->get();
+        return view('pages.admin.aset.detail', compact('aset', 'pbb', 'sporadik'));
     }
 
     public function create()
     {
-        $warga = Warga::all();
-        return view('pages.admin.aset.create', compact('warga'));
+        try {
+            $warga = Warga::all();
+            return view('pages.admin.aset.create', compact('warga'));
+        } catch (\Throwable $th) {
+            return redirect()->route('admin.aset')->with('error', 'Server Error');
+        }
     }
 
     public function store(Request $request)
     {
-        $request->validate([
-            'warga_id' => 'required',
-            'jenis_barang' => 'required',
-            'luas' => 'required',
-            'alamat' => 'required',
-        ]);
+        try {
+            $request->validate([
+                'warga_id' => 'required',
+                'jenis_barang' => 'required',
+                'luas' => 'required',
+                'alamat' => 'required',
+            ]);
 
-        $aset = Aset::where('warga_id', Hashids::decode($request->warga_id)[0])
-                    ->where('jenis_barang', $request->jenis_barang)
-                    ->where('luas', $request->luas)
-                    ->first();
+            $aset = Aset::where('warga_id', Hashids::decode($request->warga_id)[0])
+                        ->where('jenis_barang', $request->jenis_barang)
+                        ->where('luas', $request->luas)
+                        ->first();
 
-        if ($aset) {
-            return redirect()->route('admin.aset')->with('error', 'Data aset sudah ada');
+            if ($aset) {
+                return redirect()->route('admin.aset')->with('error', 'Data aset sudah ada');
+            }
+
+            $warga_id = Hashids::decode($request->warga_id)[0];
+
+            Aset::create([
+                'warga_id' => $warga_id,
+                'jenis_barang' => $request->jenis_barang,
+                'luas' => $request->luas,
+                'alamat' => $request->alamat,
+            ]);
+
+            return redirect()->route('admin.aset')->with('success', 'Data aset berhasil ditambahkan');
+        } catch (\Throwable $th) {
+            return redirect()->route('admin.aset')->with('error', 'Server Error, data aset gagal ditambahkan');
         }
-
-        $warga_id = Hashids::decode($request->warga_id)[0];
-
-        Aset::create([
-            'warga_id' => $warga_id,
-            'jenis_barang' => $request->jenis_barang,
-            'luas' => $request->luas,
-            'alamat' => $request->alamat,
-        ]);
-
-        return redirect()->route('admin.aset')->with('success', 'Data aset berhasil ditambahkan');
     }
 
     public function edit($id)
     {
-        $unhashed = Hashids::decode($id)[0];
-        $warga = Warga::all();
-        $aset = Aset::find($unhashed);
+        try {
+            $unhashed = Hashids::decode($id)[0];
+            $warga = Warga::all();
+            $aset = Aset::find($unhashed);
 
-        return view('pages.admin.aset.edit', compact('aset', 'warga'));
+            return view('pages.admin.aset.edit', compact('aset', 'warga'));
+        } catch (\Throwable $th) {
+            return redirect()->route('admin.aset')->with('error', 'Server Error');
+        }
     }
 
     public function update(Request $request, $id)
@@ -89,7 +110,7 @@ class AsetController extends Controller
 
             return redirect()->route('admin.aset')->with('success', 'Data aset berhasil diubah');
         } catch (\Throwable $th) {
-            return redirect()->route('admin.aset')->with('error', 'Data aset gagal diubah');
+            return redirect()->route('admin.aset')->with('error', 'Server Error, data aset gagal diubah');
         }
     }
 
@@ -101,7 +122,7 @@ class AsetController extends Controller
             $aset->delete();
             return redirect()->route('admin.aset')->with('success', 'Data aset berhasil dihapus');
         } catch (\Throwable $th) {
-            return redirect()->route('admin.aset')->with('error', 'Data aset gagal dihapus');
+            return redirect()->route('admin.aset')->with('error', 'Server Error, data aset gagal dihapus');
         }
     }
 }
