ec285b26949775cc1eea2774e47ca11d951941ee
fajrCode <fajri42vandi@gmail.com>
2024-08-20 13:00:13 +0700

    feat: report admin

--- resources/views/pages/admin/pengajuan/list.blade.php
+++ resources/views/pages/admin/pengajuan/list.blade.php
@@ -16,22 +16,25 @@
 
                 <div class="card">
                     <div class="card-body">
-                        <h5 class="card-title">
-                            Pengajuan Surat Warga
-                        </h5>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <h5 class="card-title">
+                                Pengajuan Surat Warga
+                            </h5>
+                            <button class="btn btn-sm btn-warning text-white" id="btn-print">Print</button>
+                        </div>
                         <!-- Table with stripped rows -->
-                        <table class="table datatable">
+                        <table id="listTable" class="table datatable">
                             <thead>
-                                <tr>
-                                    <th>Tiket Pengajuan</th>
-                                    <th>Pemohon</th>
-                                    <th>Jenis Surat</th>
-                                    <th>Tanggal</th>
-                                    <th>Perihal</th>
-                                    <th>Keterangan</th>
-                                    <th>Lampiran</th>
-                                    <th>Status</th>
-                                    <th>Action</th>
+                                <tr style="font-weight: bold">
+                                    <td>Tiket Pengajuan</td>
+                                    <td>Pemohon</td>
+                                    <td>Jenis Surat</td>
+                                    <td>Tanggal</td>
+                                    <td>Perihal</td>
+                                    <td>Keterangan</td>
+                                    <td class="no-print">Lampiran</td>
+                                    <td>Status</td>
+                                    <td class="no-print">Action</td>
                                 </tr>
                             </thead>
                             <tbody>
@@ -49,7 +52,7 @@
                                     <td>{{ $pengajuan->tanggal }}</td>
                                     <td>{{ $pengajuan->perihal }}</td>
                                     <td>{{ $pengajuan->keterangan }}</td>
-                                    <td>
+                                    <td class="no-print">
                                         <a target="_blank" href="{{ asset('lampiran/warga/pengajuan').'/'.$pengajuan->lampiran }}">Lihat Berkas <img src="{{ asset('img/pdf-download.png') }}" width="23px"></a>
                                     </td>
                                     <td>
@@ -61,7 +64,7 @@
                                         <span class="badge bg-danger">Ditolak</span>
                                         @endif
                                     </td>
-                                    <td class="text-center">
+                                    <td class="text-center no-print">
                                         @if ($pengajuan->status == 'Diproses')
                                         <div class="d-flex">
                                             <a href="{{ route('admin.pengajuan.approved', [$pengajuan->jenis_surat, $pengajuan->hashid]) }}" class="btn btn-sm btn-success text-white">Terima</a>
@@ -128,6 +131,67 @@
             const form = document.getElementById('rejected-form');
             form.action = `/admin/pengajuan/rejected/${id}`;
         })
+    </script>
+    <script>
+        function printReport() {
+            var table = document.getElementById('listTable');
+
+            // Simpan ID tabel saat ini
+            var originalId = table.id;
+
+            // Ganti ID tabel untuk tujuan print
+            table.id = 'print-table';
+
+            var printWindow = window.open('', '', 'height=800,width=1200');
+            printWindow.document.write('<html><head><title>Print Tagihan</title>');
+            printWindow.document.write('<style>');
+            printWindow.document.write('@media print { body { font-family: Arial, sans-serif; }');
+            printWindow.document.write('.header { text-align: center; margin-bottom: 20px; }');
+            printWindow.document.write('.footer { text-align: center; margin-top: 20px; }');
+            printWindow.document.write('.no-print { display: none; }'); // Menyembunyikan elemen yang tidak perlu
+            printWindow.document.write('table { width: 100%; border-collapse: collapse; margin: 20px 0; table-layout: fixed; }');
+            printWindow.document.write('th, td { border: 1px solid #ddd; text-align: center; word-wrap: break-word; padding: 8px; }');
+            printWindow.document.write('th { background-color: #f2f2f2; }');
+            printWindow.document.write('@page { size: portrait; } }');
+            printWindow.document.write('</style></head><body>');
+
+            // Header
+            printWindow.document.write('<div style="margin-bottom:3rem; padding-bottom: 1rem; text-align:center; border-bottom: 3px solid black;">');
+            printWindow.document.write('<h3 style="margin:0; padding:0;">PEMERINTAH KABUPATEN MUARO JAMBI</h3>');
+            printWindow.document.write('<h3 style="margin:0; padding:0;">KECAMATAN SEKERNAN</h3>');
+            printWindow.document.write('<h3 style="margin:0; padding:0;">KELURAHAN SENGETI</h3>');
+            printWindow.document.write('<p style="margin:0; padding:0; font-size: 13px">Jl. Kemas Tabro RT. 14 Kelurahan Sengeti Kecamatan Sekernan Kabupaten Muaro Jambi Kode Pos 36381</p>');
+            printWindow.document.write('</div>');
+
+            // Laporan
+            printWindow.document.write('<h4 style="text-align: center;">Laporan Pengajuan Surat Warga</h4>');
+            printWindow.document.write('<hr>');
+
+            // Tabel Laporan
+            printWindow.document.write(document.querySelector('#print-table').outerHTML);
+
+            // Footer
+            printWindow.document.write('<div style="text-align: right; margin-top: 4rem;">');
+            printWindow.document.write('<p style="margin-bottom:3rem; text-align: right;">Admin,</p>');
+            printWindow.document.write('<p style="margin-top:3rem;">____________</p>');
+            printWindow.document.write('</div>');
+
+            printWindow.document.write('</body></html>');
+            printWindow.document.close();
+            printWindow.focus();
+            printWindow.print();
+            printWindow.onafterprint = function() {
+            // Kembalikan ID tabel ke semula
+            table.id = originalId;
+            printWindow.close();
+            };
+        }
+
+        document.querySelector('#btn-print').addEventListener('click', function(event) {
+            event.preventDefault();
+            printReport();
+        });
+
     </script>
 
 </x-app-layout>
